2bfb83462572500c1a3066b7a54dc0e791aa9875
Onesimus Elechi Otiti <onesimus459@gmail.com>
2023-04-17 18:10:43 -0700

    C - Structures, typedef

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -12,8 +12,8 @@ void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d != NULL)
 	{
-		d->name = puple;
-		d->age = 1.3;
-		d->owner = onis;
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
 	}
 }
