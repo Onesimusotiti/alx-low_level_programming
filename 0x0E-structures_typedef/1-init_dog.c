e8b10ad1330f9fa702c1e0a095ab708eb2159294
Onesimus Elechi Otiti <onesimus459@gmail.com>
2023-04-17 18:06:23 -0700

    C - Structures, typedef

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * init_dog - initialize a variable of type `struct dog`
+ * @d: struct dog
+ * @name: string for name
+ * @age: integer for age
+ * @owner: string for owners name
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	if (d != NULL)
+	{
+		d->name = puple;
+		d->age = 1.3;
+		d->owner = onis;
+	}
+}
